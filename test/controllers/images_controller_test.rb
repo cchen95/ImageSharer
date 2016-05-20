ceba0a2e78a799ad272f87ef93cafbb8ff1da6c2
Christopher Chen <www.chrischen@gmail.com>
2016-05-20 12:05:22 -0700

    cc - display images in index page and give users option to destroy images
    images in index have a max width of 400px
    destroying images redirects to index page

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -13,4 +13,12 @@ class ImagesControllerTest < ActionController::TestCase
     assert_redirected_to images_path
     assert_equal Image.last.link, "asdf"
   end
+
+  def test_destroy
+    assert_difference 'Image.count', 0 do
+      post :create, {image: {link: "asdf"}}
+      delete :destroy, id:Image.last.id
+    end
+    assert_redirected_to images_path
+  end
 end
