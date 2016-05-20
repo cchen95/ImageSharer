f8ae31b39bdf48c23094e3877830c14b6ba3786b
Christopher Chen <www.chrischen@gmail.com>
2016-05-19 18:28:18 -0700

    cc - create ability to save user's image link in database
    link passed in by client is saved in database as part of an image entry and redirects to index page

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,4 +5,18 @@ class ImagesController < ApplicationController
 	def new
 		@image = Image.new
 	end
+
+	def create
+		@image = Image.new(image_params)
+		if @image.save
+			redirect_to images_path, notice: "Image successfully saved: #{@image.link}"
+		else
+			redirect_to images_path, notice: "Image link upload unsuccessful"
+		end
+
+	end
+
+	def image_params
+		params.require('image').permit('link')
+	end
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -5,5 +5,12 @@ class ImagesControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
-  
+
+  def test_create
+    assert_difference 'Image.count' do
+      post :create, {image: {link: "asdf"}}
+    end
+    assert_redirected_to images_path
+    assert_equal Image.last.link, "asdf"
+  end
 end
